b4ac9ed65ee9fbfe683d9d9df10556aec5a8b467
tomsun <sunjie310110@gmail.com>
2013-09-19 14:55:01 +0800

    xfce4 change wallpaper

--- commonutils/python3/WallpaperChange/change.py
+++ commonutils/python3/WallpaperChange/change.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import subprocess
+import random
 
 class WallpaperChange:
     """
@@ -21,7 +22,12 @@ class WallpaperChange:
         platformName=self.getPlatform()
         print("当前的桌面环境是"+platformName)
         if platformName=="XFCE":
-            subprocess.call("xfdesktop --reload", shell=True)
+            #subprocess.call("xfdesktop --reload", shell=True)
+            print("change wallpaper")
+            wallpaparList=self.getWallpaperList()
+            print(wallpaparList)
+            randomWallpapaper=wallpaparList[random.randrange(0,len(wallpaparList))]
+            status, output = subprocess.getstatusoutput("xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s %s" %randomWallpapaper)
         print("结束更换壁纸")
     
     def getPlatform(self):
@@ -45,7 +51,47 @@ class WallpaperChange:
     def get_output(self, cmd):
         status, output = subprocess.getstatusoutput("ps -A | grep %s" %cmd)
         return status
-
-
+    
+    def getWallpaperList(self):
+            
+        wallpaparList=[]
+        
+        paramDict={}
+        
+        #read config file
+        configFile=open("config.cfg",mode="r",encoding="utf-8")
+        for line in configFile:
+            line=line[:]
+            param=line.split("=")
+            paramName=param[0]
+            paramValue=param[1]
+            paramDict[paramName]=paramValue
+        configFile.close()
+        
+        #read wallpaperdir
+        dir=paramDict["wallpaper.dir"]
+        dir=dir.replace("\n","")
+        print(dir)
+        wallpaparList=self.getWallpaperFileList(dir,wallpaparList) 
+        return wallpaparList
+        
+    
+    def getWallpaperFileList(self, dir,wallpaparList):
+        """read dir and get wallpaper file"""
+        print("list dir imagefile---"+dir)
+        if os.path.exists(dir):
+            print("文件夹存在")
+            for imageFile in os.listdir(dir):
+                if os.path.isfile(dir+"/"+imageFile) :
+                    if imageFile.lower().endswith(".jpg"):
+                        wallpaparList.append(dir+"/"+imageFile)
+                    else:
+                        print (imageFile+"is not jpg file")
+                else:
+                    wallpaparList=wallpaparList+getWallpaperFileList(dir+"/"+imageFile,wallpaparList)
+        else:
+            print("文件夹"+dir+"不存在")
+        return wallpaparList
+        
 cls=WallpaperChange()
 cls.startChange()
