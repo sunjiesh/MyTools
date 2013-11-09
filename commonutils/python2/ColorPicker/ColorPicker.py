695dafd9659f26f12ecc0e0f848f060a4f81afb9
Tom Sun <sunjie310110@gmail.com>
2013-11-09 13:58:57 +0800

    add get pixel color

--- commonutils/python2/ColorPicker/ColorPicker.py
+++ commonutils/python2/ColorPicker/ColorPicker.py
@@ -4,8 +4,16 @@
 Module implementing Dialog.
 """
 
+from PyQt4 import QtGui
 from PyQt4.QtGui import QDialog
+from PyQt4.QtGui import QMouseEvent
+from PyQt4.QtGui import QPixmap
+from PyQt4.QtGui import QApplication
 from PyQt4.QtCore import pyqtSignature
+from PyQt4.QtCore import Qt
+
+from PIL import Image
+
 
 from Ui_ColorPicker import Ui_Dialog
 
@@ -27,3 +35,46 @@ class Dialog(QDialog, Ui_Dialog):
         """
         # TODO: not implemented yet
         raise NotImplementedError
+        
+    def mousePressEvent(self,event):  
+        if event.button()==Qt.LeftButton:  
+            print "mouse press" 
+        if event.button()==Qt.RightButton:  
+            self.close()  
+  
+    def mouseMoveEvent(self,event): 
+        """
+            鼠标移动需要把鼠标的XY值读取，通过调用PIL实现
+        """
+        if event.buttons() & Qt.LeftButton:  
+            pointX = event.globalX()
+            pointY = event.globalY()
+            print pointX
+            # img is QImage type
+            img = QPixmap.grabWindow(
+                    QApplication.desktop().winId()).toImage()
+            rgb = img.pixel(pointX, pointY)
+            #十进制
+            red10 = QtGui.qRed(rgb)
+            green10 =QtGui.qGreen(rgb)
+            blue10 = QtGui.qBlue(rgb)
+            color10="("+str(red10)+","+str(green10)+","+str(blue10)+")"
+            #十六进制
+            red16=str(hex(red10))[2]
+            if(len(red16)==1):
+                red16="0"+red16
+            green16=str(hex(green10))[2]
+            if(len(green16)==1):
+                green16="0"+green16
+            blue16=str(hex(blue10))[2]
+            if(len(blue16)==1):
+                blue16="0"+blue16
+            color16=red16+green16+blue16
+            
+
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    colorDialog = Dialog()
+    colorDialog.show()
+    sys.exit(app.exec_())
--- commonutils/python2/ColorPicker/Ui_ColorPicker.py
+++ commonutils/python2/ColorPicker/Ui_ColorPicker.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/tom/workspaces/PythonWorkspacesA/commonutils-for-python/commonutils/python2/ColorPicker/ColorPicker.ui'
 #
-# Created: Sat Nov  2 11:50:55 2013
+# Created: Sat Nov  9 13:47:55 2013
 #      by: PyQt4 UI code generator 4.10
 #
 # WARNING! All changes made in this file will be lost!
@@ -27,12 +27,20 @@ class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName(_fromUtf8("Dialog"))
         Dialog.resize(400, 300)
+        self.pushButton = QtGui.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(50, 30, 87, 27))
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.label = QtGui.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(50, 130, 201, 16))
+        self.label.setObjectName(_fromUtf8("label"))
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(_translate("Dialog", "Dialog", None))
+        self.pushButton.setText(_translate("Dialog", "Run", None))
+        self.label.setText(_translate("Dialog", "Please click the button", None))
 
 
 if __name__ == "__main__":
