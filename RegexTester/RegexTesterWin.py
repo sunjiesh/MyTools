82fb12bda0a36f62d06dfecad348390a502635ef
Tom <sunjie310110@gmail.com>
2016-05-06 15:51:44 +0800

    add listener method

--- RegexTester/RegexTesterWin.py
+++ RegexTester/RegexTesterWin.py
@@ -28,6 +28,32 @@ class Form(QWidget):
         self.formLayout.setWidget(2, QFormLayout.FieldRole, self.textResult)
         self.setLayout(self.formLayout)
 
+    def on_btnRun_clicked(self):
+        """
+        执行验证操作，读取Source和Pattern，
+        """
+        #读取Source及Pattern
+        textSource=self.textSource.toPlainText()
+        textPattern=self.textPattern.toPlainText()
+        if (textSource !="" and  textPattern!=""):
+            textSource=unicode(textSource)
+            textPattern=unicode(textPattern)
+            print("textSource="+textSource)
+            print("textPattern="+textPattern)
+            #Regex验证
+            result=""
+            compileResult = re.findall(r""+textPattern, textSource)
+        if len(compileResult)>0:
+            for resultItem in compileResult:
+                result=result+resultItem+"\n"
+            else:
+                result=u"没有找到符合条件的字符串"
+            #result赋值
+            self.textResult.setText(result)
+        else:
+            #提示弹出窗口
+            QMessageBox.information(self, "请输入Source及Pattern")
+        print("请输入Source及Pattern")
 
 if __name__ == '__main__':
     import sys
