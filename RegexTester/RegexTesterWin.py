53bc80f2f6248842f6638a9e7674173a64db3306
Tom Sun <sunjie310110@gmail.com>
2016-05-20 16:23:57 +0800

    add label

--- RegexTester/RegexTesterWin.py
+++ RegexTester/RegexTesterWin.py
@@ -10,12 +10,21 @@ from PyQt5.QtWidgets import *
 class Form(QWidget):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
-        
+
         #Compements
+        self.labelSource=QLabel()
+        self.labelSource.setObjectName("labelSource")
+        self.labelSource.setText("Source")
         self.textSource=QTextEdit()
         self.textSource.setObjectName("textSource")
+        self.labelPattern=QLabel()
+        self.labelPattern.setObjectName("labelPattern")
+        self.labelPattern.setText("Pattern")
         self.textPattern=QTextEdit()
         self.textPattern.setObjectName("textPattern")
+        self.labelResult=QLabel()
+        self.labelResult.setObjectName("labelResult")
+        self.labelResult.setText("Result")
         self.textResult=QTextBrowser()
         self.textResult.setObjectName("textaResult")
         self.btnRun = QPushButton()
@@ -35,10 +44,14 @@ class Form(QWidget):
 
         self.btnRun.clicked.connect(self.on_btnRun_clicked)
 
-        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.textSource)
-        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.textPattern)
-        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.btnRun)
-        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.textResult)
+        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.labelSource)
+        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.textSource)
+        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.labelPattern)
+        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.textPattern)
+        self.formLayout.setWidget(4, QFormLayout.FieldRole, self.btnRun)
+        self.formLayout.setWidget(5, QFormLayout.FieldRole, self.labelResult)
+        self.formLayout.setWidget(6, QFormLayout.FieldRole, self.textResult)
+
         self.setLayout(self.formLayout)
 
     def on_btnRun_clicked(self):
